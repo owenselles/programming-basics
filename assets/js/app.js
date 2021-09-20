419ef03b56503aa756d51d67611d3c29c555f24e
Owen Selles <owenselles@icloud.com>
2021-09-20 09:16:12 +0200

    better diamond

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,25 @@
 console.log('JavaScript is working!')
 
 function draw() {
-    var canvas = document.getElementById('canvas');
+    const canvas = document.getElementById('canvas');
     if (canvas.getContext){
         var x=100; var y=100;
         var w=200; var h=200;
         var ctx = canvas.getContext('2d');
-        colors=['#E3170D','#9D1309','#F22C1E'];
-        sketch(x,y,w,h,ctx,colors);
+        color=('#0089ca');
+        sketch(x,y,w,h,ctx,color);
     }
 }
 
-function sketch(x,y,w,h,ctx,colors){
-    ctx.fillStyle=colors[0];
+function sketch(x,y,w,h,ctx,color){
+    ctx.fillStyle=color;
     ctx.beginPath();
     ctx.moveTo(x,y);
     ctx.lineTo(x+w/2,y+0.7*h);
     ctx.lineTo(x+w/2,y);
     ctx.fill();
 
-    ctx.fillStyle=colors[1];
+    ctx.fillStyle=color;
     ctx.beginPath();
     ctx.moveTo(x+w/2, y);
     ctx.lineTo(x+w/2,y+0.7*h);
@@ -34,7 +34,7 @@ function sketch(x,y,w,h,ctx,colors){
     ctx.fill();
 
     // centre inverted triangle
-    ctx.fillStyle=colors[2];
+    ctx.fillStyle=color;
     ctx.beginPath();
     ctx.moveTo(x+w/4,y-0.3*h);
     ctx.lineTo(x+w/2,y);
@@ -42,12 +42,10 @@ function sketch(x,y,w,h,ctx,colors){
     ctx.fill();
 
     //Upper left triangle.
-    ctx.fillStyle=colors[0];
+    ctx.fillStyle=color;
     ctx.beginPath();
     ctx.moveTo(x+0.75*w,y-0.3*h);
     ctx.lineTo(x+w/2,y);
     ctx.lineTo(x+w,y);
     ctx.fill();
-
 }
-
