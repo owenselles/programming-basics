1757bae946a25e460167e61ba6754b9893818e15
Owen Selles <owenselles@icloud.com>
2021-09-22 11:32:53 +0200

    add 7.1 and 7.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,42 @@
-randomReturn();
+formatObject()
 
-// Return random element from array
-async function randomReturn() {
-    const lapRounds = [2.99, 3.00, 3.01, 4.01, 2.79, 2.88, 3.10, 4.12];
-    const number = Math.floor(Math.random() * lapRounds.length);
-    console.log(lapRounds[number])
+// format object into an array
+function formatObject() {
+
+    const drivers = [{
+        driver: 'Max',
+        team: 'Redbull',
+        lapRounds: [55.99, 63.00, 63.01, 54.01, 62.79, 52.88, 53.10, 54.12]
+    },
+        {
+            driver: 'Lewis',
+            team: 'Mercedes',
+            lapRounds: [55.90, 63.10, 63.1, 53.01, 64.79, 52.80, 52.09, 54.11]
+        }]
+    console.log(drivers[0].driver)
 }
 
-printRecords()
+myProfession()
 
-// print all records from array
-function printRecords() {
-    const allMyRecords = [
-        [
-            "The Who - Pinball Wizard",
-            "Rolling Stones - Exile on main street",
-            "Police - Message in a bottle"
-        ],
-        [
-            "De Dijk - Alle 40 Goed",
-            "Het Goede Doel - Belgie",
-            "Doe Maar - skunk"
-        ]
-    ];
-    for (let i = 0; i < allMyRecords.length; i++) {
-        for (let k = 0; k < allMyRecords[i].length; k++) {
-            console.log(allMyRecords[i][k])
+// print teachers
+function myProfession() {
+    const teachers = [{
+        name: "Loek",
+        profession: "Teacher",
+        brand: "Linux"
+    },
+        {
+            name: "Daan",
+            profession: "Teacher",
+            brand: "Arduino"
+        },
+        {
+            name: "Rimmert",
+            profession: "Teacher",
+            brand: "Apple"
         }
+    ]
+    for (let i = 0; i < teachers.length; i++) {
+        console.log(`${teachers[i].name} has a profession as a ${teachers[i].profession} and he likes to work on a ${teachers[i].brand} computer`)
     }
-}
-
-filteredLapRoundsWithForLoop()
-// Filter all numbers from array that are smaller than 4
-function filteredLapRoundsWithForLoop() {
-    const lapRounds = [2.99, 3.00, 3.01, 4.01, 2.79, 2.88, 3.10, 4.12];
-    const lessThanFour = lapRounds.filter(function (number) {
-        return number < 4;
-    });
-    console.log(lessThanFour);
 }
